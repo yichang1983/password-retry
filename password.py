61d29e48c8e13a8db40118f0429a62e64c800e2c
Yi-Chang Chen <yichang.thomas@gmail.com>
2021-06-15 10:36:23 +0100

    account is locked

--- password.py
+++ password.py
@@ -1,12 +1,16 @@
 password = 'a123456'
 i = 3		# password retry times
 while i > 0:
+	i = i - 1
 	pwd = input('password: ')
 	if pwd == password:
 		print('login successfully! ')
 		break		#jump out from the loop.
 	else:
-		i = i - 1
-		print('wrong password! you have', i, 'time to retry')
+		print('wrong password! ') 
+		if i > 0:
+			print('you have', i, 'time to retry')
+		else:
+			print('your account is locked')
 
 
