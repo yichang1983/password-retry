18eaf8764ab9f9839ccd1fc93f756b65b3b9b2e3
Yi-Chang Chen <yichang.thomas@gmail.com>
2021-06-15 10:00:53 +0100

    change while > 0

--- password.py
+++ password.py
@@ -1,13 +1,12 @@
 password = 'a123456'
 i = 3		# password retry times
-while True:
+while i > 0:
 	pwd = input('password: ')
 	if pwd == password:
 		print('login successfully! ')
-		break
+		break		#jump out from the loop.
 	else:
 		i = i - 1
 		print('wrong password! you have', i, 'time to retry')
-		if i == 0:
-			break
-			
+
+
